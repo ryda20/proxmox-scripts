9ce212d9f3d3e95ed228a1abf58339579d4968eb
Dien Nguyen <ddnm102@gmail.com>
2022-11-29 06:44:29 +0000

    filebrowser

--- testing/filebrowser-debian.sh
+++ testing/filebrowser-debian.sh
@@ -6,7 +6,7 @@
 
 make_service() {
 	
-	local rootdir=${1:-} # $1: define running in proxmox server or vm
+	local paras="$@"
 	local basename=$(basename $0)
 	local service_name=filebrowser
 	local service_path=/etc/systemd/system/${service_name}.service
@@ -23,7 +23,7 @@ TimeoutStartSec=0
 Restart=on-failure
 RestartSec=30s
 #ExecStartPre=
-ExecStart=filebrowser -r $rootdir
+ExecStart=filebrowser "$@"
 SyslogIdentifier=Diskutilization
 #ExecStop=
 
@@ -44,7 +44,56 @@ WantedBy=multi-user.target
 source /dev/stdin <<< "$(curl -s https://raw.githubusercontent.com/ryda20/bashlog/master/log.sh)"
 
 
-curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bash
 
-mkdir /rootdir
-make_service "/rootdir"
+main() {
+	local args=("$@")
+	local address cachedir config database port rootdir
+	while [[ $# -gt 0 ]]; do
+		case $1 in 
+		--address|-a)
+			shift; address="$1";;
+		--port|-p)
+			shift; port="$1";;
+		--cache-dir)
+			shift; cachedir="$1";;
+		--config|-c)
+			shift; config="$1";;
+		--database|-d)
+			shift; database="$1";;
+		--root|-r)
+			shift; rootdir="$1";;
+		*)
+			echo "";;
+		esac
+		shift
+	done
+
+	log "download and install"
+	curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bash
+
+
+	# default values
+	address="${address:-0.0.0.0}"
+	port="${port:-1523}"
+	cachedir="${cachedir:-}"
+	config="${config:-}"
+	database="${database:-$HOME/.filebrowser/filebrowser.db}"
+	rootdir="${rootdir:-/rootdir}"
+	#
+	mkdir -p "$rootdir"
+	mkdir -p "$HOME/.filebrowser"
+	#
+	make_service \
+		--config "$config" \
+		--address "$address" \
+		--port "$port" \
+		--cache-dir "$cachedir" \
+		--database "$database" \
+		--root "$rootdir"
+
+	# log "generate options: $HOME/.filebrowser/filebrowser.yaml"
+	log "default username and password is: admin"
+	log "database: $database"
+	log "rootdir: $rootdir"
+	log "listen on: $address:$port"
+}
